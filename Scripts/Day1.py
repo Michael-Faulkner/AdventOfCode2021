4cdacab4507f264f0b275ab150153c2fdd409d31
Michael Faulkner <michaelfaulkner2021@u.northwestern.edu>
2021-11-30 23:10:00 -0600

    Day1 code

--- Scripts/Day1.py
+++ Scripts/Day1.py
@@ -2,3 +2,35 @@ with open('Data/Day1.txt') as f:
     data = f.read().splitlines()
 
 data = [int(x) for x in data]
+
+def part1(data):
+    count = 0
+    i = 0
+
+    while i < len(data) - 1:
+        i += 1
+        second_point = data[i]
+        first_point = data[i-1]
+        if second_point > first_point:
+            count += 1
+
+    print(count)
+
+def part2(data):
+    first_index = 0
+    second_index = 1
+    count = 0
+    for i in range(len(data)):
+
+        first_window = data[first_index:first_index+3]
+        second_window = data[second_index:second_index+3]
+        if sum(second_window) > sum(first_window):
+            count += 1
+        first_index += 1
+        second_index += 1
+
+    print(count)
+
+
+#part1(data)
+part2(data)
